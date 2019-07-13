910af1267709b441768c9ba6dd25c665c4727b37
Dariusz <37488679+Volmarg@users.noreply.github.com>
2019-07-13 16:26:46 +0200

    Update MyNotesCategories.php

--- src/Entity/Modules/Notes/MyNotesCategories.php
+++ src/Entity/Modules/Notes/MyNotesCategories.php
@@ -78,6 +78,12 @@ class MyNotesCategories {
     public function getId(): ?int {
         return $this->id;
     }
+    
+    
+    public function setId(int $id): self{
+         $this->id = $id;
+         return $this;
+    }
 
     public function getIcon(): ?string {
         return $this->icon;
