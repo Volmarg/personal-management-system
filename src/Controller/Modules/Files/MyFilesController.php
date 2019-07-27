83c0a9dc933cd73582ada73b24f60045e8e959d9
Volmarg <dwlodarczyk12@gmail.com>
2019-07-27 14:49:24 +0200

    Added basic file download handling

--- src/Controller/Modules/Files/MyFilesController.php
+++ src/Controller/Modules/Files/MyFilesController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Modules\Files;
 
 use App\Controller\Files\FileUploadController;
 use App\Controller\Utils\Env;
+use App\Services\FileDownloader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,14 +20,21 @@ class MyFilesController extends AbstractController
     const KEY_FILE_NAME          = 'file_name';
     const KEY_FILE_SIZE          = 'file_size';
     const KEY_FILE_EXTENSION     = 'file_extension';
+    const KEY_FILE_FULL_PATH     = 'file_full_path';
 
     /**
      * @var Finder $finder
      */
     private $finder;
 
-    public function __construct() {
-        $this->finder = new Finder();
+    /**
+     * @var FileDownloader $file_downloader
+     */
+    private $file_downloader;
+
+    public function __construct(FileDownloader $file_downloader) {
+        $this->finder           = new Finder();
+        $this->file_downloader  = $file_downloader;
     }
 
     /**
@@ -40,13 +50,31 @@ class MyFilesController extends AbstractController
             $files = $this->getFilesFromSubdirectory($subdirectory);
         }
 
-
         $data = [
             'ajax_render'       => false,
             'files'             => $files
         ];
 
         return $this->render(static::TWIG_TEMPLATE_MY_FILES, $data);
+    }
+
+    /**
+     * @Route("download/file", name="download_file")
+     * @param Request $request
+     * @return BinaryFileResponse
+     * @throws \Exception
+     */
+    public function download(Request $request)
+    {
+
+        if( !$request->request->has(static::KEY_FILE_FULL_PATH)){
+            throw new \Exception('Request is missing key: ' . static::KEY_FILE_FULL_PATH);
+        }
+
+        $file_full_path = $request->request->get(static::KEY_FILE_FULL_PATH);
+        $file           = $this->file_downloader->download($file_full_path);
+
+        return $file;
     }
 
     private function getFilesFromSubdirectory(string $subdirectory) {
@@ -57,9 +85,14 @@ class MyFilesController extends AbstractController
         $this->finder->files()->in($searchDir);
 
         foreach ($this->finder as $index => $file) {
-            $all_files[$index][static::KEY_FILE_NAME]       = $file->getFilenameWithoutExtension();
-            $all_files[$index][static::KEY_FILE_SIZE]       = $file->getSize();
-            $all_files[$index][static::KEY_FILE_EXTENSION]  = $file->getExtension();
+
+            $all_files[$index] = [
+                static::KEY_FILE_NAME      => $file->getFilenameWithoutExtension(),
+                static::KEY_FILE_SIZE      => $file->getSize(),
+                static::KEY_FILE_EXTENSION => $file->getExtension(),
+                static::KEY_FILE_FULL_PATH => $file->getPath() . '/' . $file->getFilename()
+            ];
+
         }
 
         return $all_files;
--- src/Services/FileDownloader.php
+++ src/Services/FileDownloader.php
@@ -3,9 +3,13 @@
 namespace App\Services;
 
 use App\Controller\Utils\Env;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-class FileDownloader {
+class FileDownloader extends AbstractController {
 
     /**
      * @var string $targetDirectory
@@ -17,9 +21,24 @@ class FileDownloader {
      */
     private $finder;
 
-    public function __construct(Finder $finder) {
+    public function __construct() {
         $this->targetDirectory = Env::getUploadDir();
-        $this->finder          = $finder;
+        $this->finder          = new Finder();
+    }
+
+    /**
+     * @param $file_full_path
+     * @return BinaryFileResponse
+     * @throws \Exception
+     */
+    public function download($file_full_path)
+    {
+        if( !file_exists($file_full_path) ){
+            throw new \Exception("The file that You are trying to download, does not exist. {$file_full_path}");
+        }
+
+        $file = $this->file($file_full_path);
+        return $file;
     }
 
 }
