652e790556a591b07438d2f09a4fc4594ae3a9b3
Volmarg <dwlodarczyk12@gmail.com>
2019-09-07 18:04:56 +0200

    Adding migration and prod js for MyJobHolidays

---
+++ src/Migrations/Version20190907074501.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190907074501 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE my_job_holidays_pool (id INT AUTO_INCREMENT NOT NULL, year VARCHAR(255) NOT NULL, days_left VARCHAR(255) NOT NULL, company_name VARCHAR(255) NOT NULL, deleted TINYINT(1) NOT NULL, frozen TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE my_job_settings (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE my_job_holidays (id INT AUTO_INCREMENT NOT NULL, days_spent INT NOT NULL, information VARCHAR(255) NOT NULL, year INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE my_job_holidays_pool');
+        $this->addSql('DROP TABLE my_job_settings');
+        $this->addSql('DROP TABLE my_job_holidays');
+    }
+}
