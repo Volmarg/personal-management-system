2808137d7a82c0172dbfb6f49c579ba6c9bd1839
Volmarg <dwlodarczyk12@gmail.com>
2019-09-08 13:09:54 +0200

    Bugfix: MyImages galleries were breaking page with high amount of images in one folder

---
+++ public/assets_/static-scripts/bugfixes.js
@@ -0,0 +1,13 @@
+preventSettingMasonryGalleryAsAbsolute();
+
+/**
+ * This Bugfix is needed because masonry js gallery keeps overwriting styles for gallery
+ * so with high number of images inside div - it won't scale anymore, It cannot be changed in twig
+ * because JS overwrites it and besides i don't want to interfere with original code of that lib.
+ */
+function preventSettingMasonryGalleryAsAbsolute(){
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelector('.lightgallery .my-gallery').style = "";
+        document.querySelector('#aniimated-thumbnials').style     = "";
+    });
+}
