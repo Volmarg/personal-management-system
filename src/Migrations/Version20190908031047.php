739869537011794d772fc3cf2bab701f2a079d1e
Volmarg <dwlodarczyk12@gmail.com>
2019-09-08 06:47:15 +0200

    Pushing missing migration for MyPaymentsOwed

---
+++ src/Migrations/Version20190908031047.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190908031047 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE my_payments_owed (id INT AUTO_INCREMENT NOT NULL, target VARCHAR(255) NOT NULL, deleted TINYINT(1) NOT NULL, information VARCHAR(255) NOT NULL, date DATE DEFAULT NULL, amount INT NOT NULL, owed_by_me TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE my_payments_owed');
+    }
+}
