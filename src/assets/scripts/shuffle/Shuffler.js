e4b5b3da3fc434921df99fe9f9f280aded16a248
Volmarg <dwlodarczyk12@gmail.com>
2019-09-28 04:55:42 +0200

    Added listing tags and filtering images with the selected tag

--- src/assets/scripts/shuffle/Shuffler.js
+++ src/assets/scripts/shuffle/Shuffler.js
@@ -24,9 +24,83 @@ export default class Shuffler {
         });
 
         this.addSearchFilter();
+        this.buildTagsArrayFromTagsForImages();
     }
 
-    // Advanced filtering
+    /*******************
+     *  Tags Handling  *
+     ******************/
+    addTagsButtonsEvents() {
+        let tagsButtons = $('.shuffler-tags-options').find('button');
+        let _this   = this;
+
+        if (tagsButtons.length === 0) {
+            return;
+        }
+
+        $.each(tagsButtons, (index, button) => {
+            $(button).on('click', (event) => {
+                _this.handleTagClick(event);
+            });
+        });
+    };
+
+    handleTagClick(event) {
+        let btn      = event.currentTarget;
+        let isActive = $(btn).hasClass('active');
+        let btnGroup = $(btn).attr('data-group');
+
+        // 'lets only one filter button be active at a time.
+        this.removeActiveClassFromChildrenOnTagClick(btn.parentNode);
+
+        let filterGroup;
+
+        if (isActive) {
+            $(btn).removeClass('active');
+            filterGroup = Shuffle.ALL_ITEMS;
+        } else {
+            $(btn).addClass('active');
+            filterGroup = btnGroup;
+        }
+
+        this.shuffle.filter(filterGroup);
+    };
+
+    removeActiveClassFromChildrenOnTagClick(parent) {
+        let children = parent.children;
+        for (var i = children.length - 1; i >= 0; i--) {
+            $(children[i]).removeClass('active');
+        }
+    };
+
+    buildTagsArrayFromTagsForImages(){
+        let allIShuffleItems = this.shuffle.items;
+        let _this = this;
+
+        this.tags = [];
+
+        $.each(allIShuffleItems, (index, item) => {
+
+           let targetImage    = $(item.element);
+           let tagsJsonString = $(targetImage).attr('data-groups');
+
+           // skip empty tags
+           if( "" != tagsJsonString ){
+               let tagsArray = JSON.parse(tagsJsonString);
+               _this.tags = _this.tags.concat(tagsArray);
+           }
+
+        });
+
+        // will get only unique tags (distinct)
+        this.tags = Array.from(new Set(this.tags));
+
+        return this.tags;
+    }
+
+    /*******************
+     * Search Handling  *
+     ******************/
     addSearchFilter() {
         const searchInput = document.querySelector(this.searchSelector);
         if (!searchInput) {
--- src/assets/scripts/shuffle/index.js
+++ src/assets/scripts/shuffle/index.js
@@ -17,7 +17,8 @@ export default (function () {
             },
             classes: {
                 itemSelector   : '.grid-item',
-                searchSelector : '.js-shuffle-search'
+                searchSelector : '.js-shuffle-search',
+                tagsHolder     : '.shuffler-tags-options'
             },
             other: {
                 galleryHolder  : "aniimated-thumbnials",
@@ -42,8 +43,31 @@ export default (function () {
 
                 let shuffler = new Shuffler(domGalleryHolder, itemSelector, searchSelector);
                 shuffler.init();
+
+                let tagsArray = shuffler.buildTagsArrayFromTagsForImages();
+                _this.appendTagsToFilter(tagsArray);
+
+                shuffler.addTagsButtonsEvents();
+
             });
 
+        },
+        appendTagsToFilter: function(tagsArray){
+            let domTagsHolder = $(this.selectors.classes.tagsHolder);
+
+            //append each tag as option
+            $.each(tagsArray, (index, tag) => {
+                let button = $('<button>');
+                button.addClass('btn btn-sm btn-primary');
+                button.css({
+                    "margin": "2px",
+                    "height": '30px'
+                });
+                button.attr('data-group', tag);
+                button.html(tag);
+
+                $(domTagsHolder).append(button);
+            });
         }
     };
 
