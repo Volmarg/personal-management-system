b31f04eed373cafd910b3f7adbf3f6837f04e631
Volmarg <dwlodarczyk12@gmail.com>
2019-10-09 17:24:49 +0200

    Bugfix: goals payments bars were not rendered on ajax load

--- src/assets/scripts/loading-bar/index.js
+++ src/assets/scripts/loading-bar/index.js
@@ -6,13 +6,17 @@ export default (function () {
             'loading-bar': 'ldBar',
         },
         init: function () {
-            this.calculateDataValue();
-        },
-        calculateDataValue: function () {
             $('.' + this.classes["loading-bar"]).each((index, element) => {
-                let data_value = $(element).find('.pool-progress').html() / $(element).find('.pool-target').html() * 100;
-                $(element).attr('data-value', data_value.toPrecision(2));
+                this.calculateDataValue(element);
+                this.renderLoadingBar(element);
             });
+        },
+        calculateDataValue: function (element) {
+            let data_value = $(element).find('.pool-progress').html() / $(element).find('.pool-target').html() * 100;
+            $(element).attr('data-value', data_value.toPrecision(2));
+        },
+        renderLoadingBar: function(element){
+            window.ldBar(element);
         }
     };
 
