f7821413b15093b77be298640aac462ce2719ebd
root <dwlodarczyk12@gmail.com>
2019-11-21 17:36:18 +0100

    Bugfix: could not save larger amount of contacts in db

--- src/Migrations/Version20191110042650.php
+++ src/Migrations/Version20191110042650.php
@@ -22,7 +22,7 @@ final class Version20191110042650 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE my_contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, contacts VARCHAR(255) NOT NULL, deleted TINYINT(1) NOT NULL, image_path VARCHAR(255) DEFAULT NULL, name_background_color VARCHAR(255) NOT NULL, description_background_color VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE my_contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, contacts LONGTEXT NOT NULL, deleted TINYINT(1) NOT NULL, image_path VARCHAR(255) DEFAULT NULL, name_background_color VARCHAR(255) NOT NULL, description_background_color VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE my_contact_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, image_path VARCHAR(255) NOT NULL, deleted TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('DROP TABLE my_contacts');
         $this->addSql('DROP TABLE my_contacts_groups');
