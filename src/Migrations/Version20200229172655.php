dc5c40c7144cf814f1b4a6e19aef72f65399356e
Volmarg <dwlodarczyk12@gmail.com>
2020-03-17 20:50:11 +0100

    Add missing migration

---
+++ src/Migrations/Version20200229172655.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200229172655 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE locked_resource (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, record VARCHAR(255) NOT NULL, UNIQUE INDEX unique_record (type, target, record), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE app_user ADD lock_password VARCHAR(255) DEFAULT NULL');
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
