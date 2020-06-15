8534c57842b1ce162b7d6fca181a290e19bc76ac
Volmarg <dwlodarczyk12@gmail.com>
2020-06-15 15:37:08 +0200

    Resolve few twig deprecations + add composer patches package

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
