26b1d568cbb75e40453a42f7481b25b8a299c950
Volmarg <dwlodarczyk12@gmail.com>
2020-10-27 17:27:36 +0100

    Add missing migration with indexes for few tables (sqls optimization)

---
+++ src/Migrations/Version20201018124944.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Controller\Core\Migrations;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201018124944 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql(Migrations::buildSqlExecutedIfConstraintDoesNotExist(
+            Migrations::CONSTRAINT_TYPE_INDEX,
+            'file_path_index',
+            'CREATE INDEX file_path_index ON files_tags (full_file_path)'
+        ));
+
+        $this->addSql(Migrations::buildSqlExecutedIfConstraintDoesNotExist(
+            Migrations::CONSTRAINT_TYPE_INDEX,
+            'locked_resource_index',
+            'CREATE INDEX locked_resource_index ON locked_resource(type, record, target)'
+        ));
+
+        $this->addSql(Migrations::buildSqlExecutedIfConstraintDoesNotExist(
+            Migrations::CONSTRAINT_TYPE_INDEX,
+            'locked_resource_index',
+            'CREATE INDEX my_note_category_index ON my_note_category(id)'
+        ));
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // no way back
+    }
+}
