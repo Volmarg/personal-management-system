5a8bd8819c2bbfd786f039a0db97c4bb94aa0e30
Volmarg <dwlodarczyk12@gmail.com>
2021-07-17 07:26:13 +0200

    Optimize docker installation

---
+++ docker-first-run.sh
@@ -0,0 +1,3 @@
+composer install;               # install all composer packages
+composer pms-installer-docker;  # preconfigure system
+chmod 777 .env;                 # set .env file permission to let installer setting `done` flag
