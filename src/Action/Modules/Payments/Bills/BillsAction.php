73eb41f3921c106883b43519fa2b6ad8490fd073
Volmarg <dwlodarczyk12@gmail.com>
2024-12-14 15:49:02 +0100

    :sparkles: Make payment bills works

---
+++ src/Action/Modules/Payments/Bills/BillsAction.php
@@ -0,0 +1,146 @@
+<?php
+
+
+namespace App\Action\Modules\Payments\Bills;
+
+
+use App\Annotation\System\ModuleAnnotation;
+use App\Controller\Modules\ModulesController;
+use App\Controller\Modules\Payments\MyPaymentsBillsController;
+use App\Entity\Modules\Payments\MyPaymentsBills;
+use App\Response\Base\BaseResponse;
+use App\Services\RequestService;
+use App\Services\TypeProcessor\ArrayHandler;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/module/payment/bills", name: "module.payment.bills.")]
+#[ModuleAnnotation(values: ["name" => ModulesController::MODULE_NAME_PAYMENTS])]
+class BillsAction extends AbstractController {
+
+
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly MyPaymentsBillsController $billsController
+    ) {
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("", name: "new", methods: [Request::METHOD_POST])]
+    public function new(Request $request): JsonResponse
+    {
+        return $this->createOrUpdate($request)->toJsonResponse();
+    }
+
+    /**
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("/all", name: "get_all", methods: [Request::METHOD_GET])]
+    public function getAll(): JsonResponse
+    {
+        $bills       = $this->billsController->getAllNotDeleted();
+        $entriesData = [];
+        foreach ($bills as $bill) {
+            $elements = [];
+            foreach ($bill->getItem() as $item) {
+                if ($item->isDeleted()) {
+                    continue;
+                }
+
+                $elements[] = [
+                    'id'     => $item->getId(),
+                    'amount' => $item->getAmount(),
+                    'name'   => $item->getName(),
+                    'date'   => $item->getDate()?->format('Y-m-d'),
+                ];
+            }
+
+            $entriesData[] = [
+                'id'            => $bill->getId(),
+                'name'          => $bill->getName(),
+                'startDate'     => $bill->getStartDate()?->format("Y-m-d"),
+                'endDate'       => $bill->getEndDate()?->format("Y-m-d"),
+                'information'   => $bill->getInformation(),
+                'plannedAmount' => $bill->getPlannedAmount(),
+                'elements'      => $elements,
+            ];
+        }
+
+        $response = BaseResponse::buildOkResponse();
+        $response->setAllRecordsData($entriesData);
+
+        return $response->toJsonResponse();
+    }
+
+    /**
+     * @param MyPaymentsBills $bill
+     * @param Request         $request
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("/{id}", name: "update", methods: [Request::METHOD_PATCH])]
+    public function update(MyPaymentsBills $bill, Request $request): JsonResponse
+    {
+        return $this->createOrUpdate($request, $bill)->toJsonResponse();
+    }
+
+    /**
+     * @param MyPaymentsBills $bill
+     *
+     * @return JsonResponse
+     */
+    #[Route("/{id}", name: "remove", methods: [Request::METHOD_DELETE])]
+    public function remove(MyPaymentsBills $bill): JsonResponse
+    {
+        $bill->setDeleted(true);
+        $this->em->persist($bill);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse()->toJsonResponse();
+    }
+
+    /**
+     * @param Request              $request
+     * @param MyPaymentsBills|null $bill
+     *
+     * @return BaseResponse
+     * @throws Exception
+     */
+    private function createOrUpdate(Request $request, ?MyPaymentsBills $bill = null): BaseResponse
+    {
+        if (!$bill) {
+            $bill = new MyPaymentsBills();
+        }
+
+        $dataArray       = RequestService::tryFromJsonBody($request);
+        $startDateString = ArrayHandler::get($dataArray, 'startDate');
+        $endDateString   = ArrayHandler::get($dataArray, 'endDate');
+        $name            = ArrayHandler::get($dataArray, 'name');
+        $information     = ArrayHandler::get($dataArray, 'information');
+        $plannedAmount   = ArrayHandler::get($dataArray, 'plannedAmount');
+
+        $bill->setStartDate(new DateTime($startDateString));
+        $bill->setEndDate(new DateTime($endDateString));
+        $bill->setInformation($information);
+        $bill->setPlannedAmount((int)$plannedAmount);
+        $bill->setName($name);
+
+        $this->em->persist($bill);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse();
+    }
+
+}
---
+++ src/Action/Modules/Payments/Bills/ItemsAction.php
@@ -0,0 +1,107 @@
+<?php
+
+
+namespace App\Action\Modules\Payments\Bills;
+
+
+use App\Annotation\System\ModuleAnnotation;
+use App\Controller\Modules\ModulesController;
+use App\Entity\Modules\Payments\MyPaymentsBills;
+use App\Entity\Modules\Payments\MyPaymentsBillsItems;
+use App\Response\Base\BaseResponse;
+use App\Services\RequestService;
+use App\Services\TypeProcessor\ArrayHandler;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/module/payment/bills/items", name: "module.payment.bills.items.")]
+#[ModuleAnnotation(values: ["name" => ModulesController::MODULE_NAME_PAYMENTS])]
+class ItemsAction extends AbstractController {
+
+
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    ) {
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("", name: "new", methods: [Request::METHOD_POST])]
+    public function new(Request $request): JsonResponse
+    {
+        return $this->createOrUpdate($request)->toJsonResponse();
+    }
+
+    /**
+     * @param MyPaymentsBillsItems $item
+     * @param Request              $request
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("/{id}", name: "update", methods: [Request::METHOD_PATCH])]
+    public function update(MyPaymentsBillsItems $item, Request $request): JsonResponse
+    {
+        return $this->createOrUpdate($request, $item)->toJsonResponse();
+    }
+
+    /**
+     * @param MyPaymentsBillsItems $item
+     *
+     * @return JsonResponse
+     */
+    #[Route("/{id}", name: "remove", methods: [Request::METHOD_DELETE])]
+    public function remove(MyPaymentsBillsItems $item): JsonResponse
+    {
+        $item->setDeleted(true);
+        $this->em->persist($item);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse()->toJsonResponse();
+    }
+
+    /**
+     * @param Request                   $request
+     * @param MyPaymentsBillsItems|null $item
+     *
+     * @return BaseResponse
+     * @throws Exception
+     */
+    private function createOrUpdate(Request $request, ?MyPaymentsBillsItems $item = null): BaseResponse
+    {
+        if (!$item) {
+            $item = new MyPaymentsBillsItems();
+        }
+
+        $dataArray  = RequestService::tryFromJsonBody($request);
+        $dateString = ArrayHandler::get($dataArray, 'date');
+        $amount     = ArrayHandler::get($dataArray, 'amount');
+        $billId     = ArrayHandler::get($dataArray, 'billId');
+        $name       = ArrayHandler::get($dataArray, 'name');
+
+        $bill = $this->em->find(MyPaymentsBills::class, $billId);
+        if (is_null($bill)) {
+            throw new Exception("No bill was found for id: {$billId}");
+        }
+
+        $item->setDate(new DateTime($dateString));
+        $item->setName($name);
+        $item->setAmount((int)$amount);
+        $item->setBill($bill);
+
+        $this->em->persist($item);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse();
+    }
+
+}
