9848a9f1494c331eb26a81dbca13c5ca3e826010
Volmarg <dwlodarczyk12@gmail.com>
2025-01-07 14:39:05 +0100

    :sparkles: Passwords module - make it work

--- src/Action/Modules/Passwords/MyPasswordsAction.php
+++ src/Action/Modules/Passwords/MyPasswordsAction.php
@@ -2,182 +2,134 @@
 
 namespace App\Action\Modules\Passwords;
 
-use App\Controller\Core\AjaxResponse;
-use App\Controller\Core\Application;
-use App\Controller\Core\Controllers;
-use App\Controller\Core\Repositories;
-use Doctrine\ORM\Mapping\MappingException;
+use App\Controller\Modules\ModulesController;
+use App\Controller\Modules\Passwords\MyPasswordsController;
+use App\Entity\Modules\Passwords\MyPasswords;
+use App\Entity\Modules\Passwords\MyPasswordsGroups;
+use App\Response\Base\BaseResponse;
+use App\Services\RequestService;
+use App\Services\TypeProcessor\ArrayHandler;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use SpecShaper\EncryptBundle\Encryptors\EncryptorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Annotation\System\ModuleAnnotation;
 
-/**
- * Class MyPasswordsAction
- * @package App\Action\Modules\Passwords
- * @ModuleAnnotation(
- *     name=App\Controller\Modules\ModulesController::MODULE_NAME_PASSWORDS
- * )
- */
+#[Route("/module/passwords", name: "module.passwords.")]
+#[ModuleAnnotation(values: ["name" => ModulesController::MODULE_NAME_PASSWORDS])]
 class MyPasswordsAction extends AbstractController {
 
-    /**
-     * @var Application $app
-     */
-    private Application $app;
-
-    /**
-     * @var EncryptorInterface
-     */
-    private EncryptorInterface $encryptor;
-
-    /**
-     * @var Controllers $controller
-     */
-    private Controllers $controller;
-
-    public function __construct(Application $app, EncryptorInterface $encryptor, Controllers $controllers) {
-        $this->app        = $app;
-        $this->controller = $controllers;
-        $this->encryptor  = $encryptor;
-    }
-
-
-    /**
-     * @Route("/my-passwords", name="my-passwords")
-     * @param Request $request
-     * @return Response
-     * @throws Exception
-     */
-    public function display(Request $request) {
-        $passwordForm = $this->app->forms->myPasswordForm();
-        $this->addFormDataToDB($passwordForm, $request);
-
-        if (!$request->isXmlHttpRequest()) {
-            return $this->renderTemplate(false);
-        }
-
-        $templateContent = $this->renderTemplate(true)->getContent();
-        $ajaxResponse    = new AjaxResponse("", $templateContent);
-        $ajaxResponse->setCode(Response::HTTP_OK);
-        $ajaxResponse->setPageTitle($this->getPasswordsPageTitle());
-
-        return $ajaxResponse->buildJsonResponse();
+    public function __construct(
+        private readonly MyPasswordsController  $passwordsController,
+        private readonly EntityManagerInterface $em
+    ) {
     }
 
     /**
-     * @Route("/my-passwords/remove/", name="my-passwords-remove")
-     * @param Request $request
-     * @return Response
-     * @throws Exception
-     */
-    public function remove(Request $request): Response
-    {
-
-        $response = $this->app->repositories->deleteById(
-            Repositories::MY_PASSWORDS_REPOSITORY_NAME,
-            $request->request->get('id')
-        );
-
-        $message = $response->getContent();
-
-        if ($response->getStatusCode() == 200) {
-            $renderedTemplate = $this->renderTemplate(true, true);
-            $templateContent  = $renderedTemplate->getContent();
-
-            return AjaxResponse::buildJsonResponseForAjaxCall(200, $message, $templateContent);
-        }
-        return AjaxResponse::buildJsonResponseForAjaxCall(500, $message);
-    }
-
-    /**
-     * @Route("my-passwords/update/" ,name="my-passwords-update")
-     * @param Request $request
      * @return JsonResponse
-     * @throws MappingException
      */
-    public function update(Request $request): JsonResponse
+    #[Route("/all", name: "get_all", methods: [Request::METHOD_GET])]
+    public function getAll(): JsonResponse
     {
-        $parameters = $request->request->all();
-        $entityId   = $parameters['id'];
+        $passwords = $this->passwordsController->findAllNotDeleted();
 
-        $entity   = $this->controller->getMyPasswordsController()->findPasswordEntityById($entityId);
-        $response = $this->app->repositories->update($parameters, $entity);
-
-        return AjaxResponse::initializeFromResponse($response)->buildJsonResponse();
-    }
-
-    /**
-     * @param bool $ajaxRender
-     * @param bool $skipRewritingTwigVarsToJs
-     * @return Response
-     */
-    protected function renderTemplate(bool $ajaxRender = false, bool $skipRewritingTwigVarsToJs = false): Response
-    {
-
-        $passwordForm  = $this->app->forms->myPasswordForm();
-        $formView      = $passwordForm->createView();
-        $passwords     = $this->controller->getMyPasswordsController()->findAllNotDeleted();
-        $groups        = $this->controller->getMyPasswordsGroupsController()->findAllNotDeleted();
-
-        return $this->render('modules/my-passwords/my-passwords.html.twig', [
-            'form'                           => $formView,
-            'ajax_render'                    => $ajaxRender,
-            'passwords'                      => $passwords,
-            'groups'                         => $groups,
-            'skip_rewriting_twig_vars_to_js' => $skipRewritingTwigVarsToJs,
-            'page_title'                     => $this->getPasswordsPageTitle(),
-        ]);
-
-    }
-
-    /**
-     * @Route("/my-passwords/get-password/{id}" ,name="my-passwords-get-password")
-     * @param $id
-     * @return JsonResponse
-     * @throws \Doctrine\DBAL\Driver\Exception
-     * @throws Exception
-     */
-    public function getPasswordForId($id): JsonResponse
-    {
-        try {
-            $encryptedPassword = $this->controller->getMyPasswordsController()->getPasswordForId($id);
-            $decryptedPassword = $this->encryptor->decrypt($encryptedPassword);
-            return AjaxResponse::buildJsonResponseForAjaxCall(200, "", null, $decryptedPassword);
-        } catch (Exception $e) {
-            $exceptionMessage = $e->getMessage();
-            return AjaxResponse::buildJsonResponseForAjaxCall(500, $exceptionMessage);
-        }
-    }
-
-    /**
-     * @param $form
-     * @param $request
-     */
-    protected function addFormDataToDB($form, $request) {
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted($request) && $form->isValid()) {
-            $formData = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formData);
-            $em->flush();
+        $entriesData = [];
+        foreach ($passwords as $password) {
+            $entriesData[] = [
+                'id'          => $password->getId(),
+                'login'       => $password->getLogin(),
+                'url'         => $password->getUrl(),
+                'description' => $password->getDescription(),
+                'groupId'     => $password->getGroup()->getId(),
+                'groupName'   => $password->getGroup()->getName(),
+                'password'    => $password->getPassword(),
+            ];
         }
 
+        $response = BaseResponse::buildOkResponse();
+        $response->setAllRecordsData($entriesData);
+
+        return $response->toJsonResponse();
     }
 
     /**
-     * Will return passwords page title
+     * @param Request $request
      *
-     * @return string
+     * @return JsonResponse
+     * @throws Exception
      */
-    private function getPasswordsPageTitle(): string
+    #[Route("", name: "new", methods: [Request::METHOD_POST])]
+    public function new(Request $request): JsonResponse
     {
-        return $this->app->translator->translate('passwords.title');
+        return $this->createOrUpdate($request)->toJsonResponse();
+    }
+
+    /**
+     * @param MyPasswords $password
+     * @param Request     $request
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    #[Route("/{id}", name: "update", methods: [Request::METHOD_PATCH])]
+    public function update(MyPasswords $password, Request $request): JsonResponse
+    {
+        return $this->createOrUpdate($request, $password)->toJsonResponse();
+    }
+
+    /**
+     * @param MyPasswords $password
+     *
+     * @return JsonResponse
+     */
+    #[Route("/{id}", name: "remove", methods: [Request::METHOD_DELETE])]
+    public function remove(MyPasswords $password): JsonResponse
+    {
+        $password->setDeleted(true);
+        $this->em->persist($password);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse()->toJsonResponse();
+    }
+
+    /**
+     * @param Request          $request
+     * @param MyPasswords|null $password
+     *
+     * @return BaseResponse
+     * @throws Exception
+     */
+    private function createOrUpdate(Request $request, ?MyPasswords $password = null): BaseResponse
+    {
+        $isNew = is_null($password);
+        if ($isNew) {
+            $password = new MyPasswords();
+        }
+
+        $dataArray      = RequestService::tryFromJsonBody($request);
+        $login          = ArrayHandler::get($dataArray, 'login');
+        $url            = ArrayHandler::get($dataArray, 'url');
+        $description    = ArrayHandler::get($dataArray, 'description');
+        $groupId        = ArrayHandler::get($dataArray, 'groupId');
+        $passwordString = ArrayHandler::get($dataArray, 'password');
+
+        $groupEntity = $this->em->find(MyPasswordsGroups::class, $groupId);
+        if (is_null($groupEntity)) {
+            throw new Exception("No password group exists for this id");
+        }
+
+        $password->setLogin($login);
+        $password->setUrl($url);
+        $password->setDescription($description);
+        $password->setGroup($groupEntity);
+        $password->setPassword($passwordString);
+
+        $this->em->persist($password);
+        $this->em->flush();
+
+        return BaseResponse::buildOkResponse();
     }
 
 }
