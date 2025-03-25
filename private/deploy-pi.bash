010a133b2f6b2f331c45fab24aa0ccba11fb405c
Volmarg <dwlodarczyk12@gmail.com>
2025-03-25 17:29:59 +0100

    :technologist: Tweak pi deployment file

--- private/deploy-pi.bash
+++ private/deploy-pi.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+CONTAINER_NAME='pms-php-fpm'
+
 DIR="/home/volmarg/Partitions/Apps/pms/personal-management-system"
 IP="192.168.0.38"
 rsync -h -v -r -P -t \
@@ -15,3 +17,9 @@ rsync -h -v -r -P -t \
 
 ssh "$IP" "sudo chgrp www-data $DIR -R"
 ssh "$IP" "sudo chmod 775 $DIR -R"
+
+ssh "$IP" "sudo chmod 775 $DIR -R"
+
+ssh "$IP" "docker exec $CONTAINER_NAME composer dump-autoload"
+ssh "$IP" "docker exec $CONTAINER_NAME bin/console cache:clear"
+ssh "$IP" "docker exec $CONTAINER_NAME bin/console cache:warmup"
