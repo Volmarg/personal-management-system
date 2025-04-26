a175a3592efb421ae0f32ddcc4fadbcab156ed3d
Volmarg <dwlodarczyk12@gmail.com>
2025-04-26 11:42:05 +0200

    :bug: Correct paths for deploy script for

--- private/deploy.bash
+++ private/deploy.bash
@@ -1,20 +1,23 @@
 #!/bin/bash
 CONTAINER_NAME='pms-php-fpm'
 
-DIR="/home/volmarg/Partitions/Apps/pms/personal-management-system";
 HOST_PI="192.168.0.38";
 HOST_DEMO='private-host';
+
 USED_HOST='';
+DIR='';
 
 if [ "$1" == "pi" ]; then
+  DIR="/home/volmarg/Partitions/Apps/pms/personal-management-system";
   USED_HOST="$HOST_PI";
 fi;
 
 if [ "$1" == "demo" ]; then
+  DIR="/var/www/pms/personal-management-system";
   USED_HOST="$HOST_DEMO";
 fi;
 
-if [ -z "$USED_HOST" ]; then
+if [[ -z "$USED_HOST" || -z "$DIR" ]]; then
   printf "Invalid target host, got '$1'"
   exit 1;
 fi;
