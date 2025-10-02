04bb5da390d9a4c3ea4853c5ac9d40e0ca37ba49
Volmarg <dwlodarczyk12@gmail.com>
2025-10-02 14:20:00 +0200

    :fire: Wipe legacy ConfigLoaderSession

--- src/Controller/Core/ConfigLoaders.php
+++ src/Controller/Core/ConfigLoaders.php
@@ -5,16 +5,10 @@ namespace App\Controller\Core;
 
 
 use App\Services\ConfigLoaders\ConfigLoaderSecurity;
-use App\Services\ConfigLoaders\ConfigLoaderSession;
 use App\Services\ConfigLoaders\ConfigLoaderSystem;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConfigLoaders extends AbstractController {
-
-    /**
-     * @var ConfigLoaderSession $configLoaderSession
-     */
-    private ConfigLoaderSession $configLoaderSession;
 
     /**
      * @var ConfigLoaderSecurity $configLoaderSecurity
@@ -25,13 +19,6 @@ class ConfigLoaders extends AbstractController {
      * @var ConfigLoaderSystem $configLoaderSystem
      */
     private ConfigLoaderSystem $configLoaderSystem;
-
-    /**
-     * @return ConfigLoaderSession
-     */
-    public function getConfigLoaderSession(): ConfigLoaderSession {
-        return $this->configLoaderSession;
-    }
 
     /**
      * @return ConfigLoaderSecurity
@@ -50,12 +37,10 @@ class ConfigLoaders extends AbstractController {
     }
 
     public function __construct(
-        ConfigLoaderSession  $configLoaderSession,
         ConfigLoaderSecurity $configLoaderSecurity,
         ConfigLoaderSystem   $configLoaderSystem
     )
     {
-        $this->configLoaderSession  = $configLoaderSession;
         $this->configLoaderSecurity = $configLoaderSecurity;
         $this->configLoaderSystem   = $configLoaderSystem;
     }
