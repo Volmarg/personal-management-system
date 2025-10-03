d3c717cc8e93043b67aad91c6c3ed7ef25e7feb5
Volmarg <dwlodarczyk12@gmail.com>
2025-10-03 07:49:15 +0200

    :bug: Reminders command not marking reminder as handled

--- src/Command/Module/Schedules/SendRemindersCommand.php
+++ src/Command/Module/Schedules/SendRemindersCommand.php
@@ -5,9 +5,11 @@ namespace App\Command\Module\Schedules;
 use App\DTO\NotificationDto;
 use App\DTO\Settings\Notifications\ConfigDTO;
 use App\DTO\Settings\SettingNotificationDTO;
+use App\Entity\Modules\Schedules\MyScheduleReminder;
 use App\Repository\Modules\Schedules\MyScheduleRepository;
 use App\Services\External\DiscordService;
 use App\Services\Settings\SettingsLoader;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use Psr\Log\LoggerInterface;
@@ -32,18 +34,20 @@ class SendRemindersCommand extends Command
     private string $reminderUserName;
 
     /**
-     * @param MyScheduleRepository  $myScheduleRepository
-     * @param SettingsLoader        $settingsLoader
-     * @param LoggerInterface       $logger
-     * @param ParameterBagInterface $parameterBag
-     * @param DiscordService        $discordService
+     * @param MyScheduleRepository   $myScheduleRepository
+     * @param SettingsLoader         $settingsLoader
+     * @param LoggerInterface        $logger
+     * @param ParameterBagInterface  $parameterBag
+     * @param DiscordService         $discordService
+     * @param EntityManagerInterface $entityManager
      */
     public function __construct(
-        private readonly MyScheduleRepository $myScheduleRepository,
-        private readonly SettingsLoader       $settingsLoader,
-        private readonly LoggerInterface      $logger,
-        readonly ParameterBagInterface        $parameterBag,
-        private readonly DiscordService       $discordService
+        private readonly MyScheduleRepository   $myScheduleRepository,
+        private readonly SettingsLoader         $settingsLoader,
+        private readonly LoggerInterface        $logger,
+        readonly ParameterBagInterface          $parameterBag,
+        private readonly DiscordService         $discordService,
+        private readonly EntityManagerInterface $entityManager,
     ) {
         parent::__construct(self::$defaultName);
         $this->reminderUserName = $parameterBag->get('project.name') . " Reminder";
@@ -79,6 +83,12 @@ class SendRemindersCommand extends Command
                 $title = "Schedule: {$scheduleDto->getTitle()} - {$scheduleDto->getDate()}";
                 $notificationDto = new NotificationDto(title: $title, message: $scheduleDto->getBody(), username: $this->reminderUserName);
                 $this->handleNotifications($notificationDto, $notificationConfig->getConfig());
+
+                $reminder = $this->entityManager->find(MyScheduleReminder::class, $scheduleDto->getReminderId());
+                $reminder->setProcessed(true);
+
+                $this->entityManager->persist($reminder);
+                $this->entityManager->flush();
             }
         } catch (Exception $e) {
             $this->logger->critical($e->getMessage(), ['exception' => $e]);
