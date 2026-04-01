6641f1822a2dc480f1b2e409969d4ac0b97597b5
Volmarg <dwlodarczyk12@gmail.com>
2026-04-01 14:51:19 +0200

    :technologist: Define dev debug route for backend

---
+++ src/Action/DevAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Action;
+
+use App\Response\Base\BaseResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DevAction extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    public function debug(): JsonResponse
+    {
+        return BaseResponse::buildOkResponse()->toJsonResponse();
+    }
+
+}
