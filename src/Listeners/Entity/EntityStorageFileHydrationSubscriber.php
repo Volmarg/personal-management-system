bf764317a9e118108cf97b844077f32b32088b46
Volmarg <dwlodarczyk12@gmail.com>
2026-06-23 14:18:02 +0200

    🐛 StorageFile hydration fails if no entity is related

--- src/Listeners/Entity/EntityStorageFileHydrationSubscriber.php
+++ src/Listeners/Entity/EntityStorageFileHydrationSubscriber.php
@@ -67,6 +67,10 @@ class EntityStorageFileHydrationSubscriber implements EventSubscriber
             }
         }
 
+        if (!isset($entity)) {
+            return;
+        }
+
         $this->repository->handleRelationWithStorageFile($entity->getStorageFiles(), $entity);
     }
 }
